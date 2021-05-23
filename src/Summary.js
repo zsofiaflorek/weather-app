681e53b2a7d96deb4d240d64c1b59206dbf84bbb
Zsofia Florek <58311322+zsofiaflorek@users.noreply.github.com>
2021-05-23 19:48:10 +0200

    Add icons to Summary

--- src/Summary.js
+++ src/Summary.js
@@ -1,14 +1,15 @@
 import './Summary.css'
 import imageSunny from './Clear.png'
 
+
 export const Summary = ({temperature, date, location, weatherLabel}) => {
     return (
         <div className='summaryPanel'>
             <div> <img src={imageSunny} alt='Weather icon'/> </div>
             <div className='temperature'>{Math.round(temperature)}<small>°C</small></div>
             <div className='weatherLabel'>{weatherLabel}</div>
-            <div>Today {date}</div>
-            <div>{location}</div>
+            <div>Today • {date}</div>
+            <div><i className="fas fa-map-marker-alt"/> {location}</div>
         </div>
     )
 
