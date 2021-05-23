a96a9d20dee18913df5207be9bd994c997e81326
Zsofia Florek <58311322+zsofiaflorek@users.noreply.github.com>
2021-05-23 19:31:01 +0200

    Create basic structure for Summary

--- src/WeatherDisplay.js
+++ src/WeatherDisplay.js
@@ -1,5 +1,17 @@
+import {Summary} from "./Summary";
+
+
 
 export const WeatherDisplay = ({weather}) => {
-        return weather.time;
+    const currentWeather=weather.consolidated_weather[0];
+    return (
+        <div>
+            <Summary date={currentWeather.applicable_date}
+                     temperature={currentWeather.the_temp}
+                     location={weather.title}
+                     weatherLabel={currentWeather.weather_state_name}
+            />
+        </div>
+    )
 };
 
