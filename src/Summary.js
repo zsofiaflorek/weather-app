63ef69a4d6f9029eb1fe4ec72e2656e49abe04dc
Zsofia Florek <58311322+zsofiaflorek@users.noreply.github.com>
2021-05-23 20:07:13 +0200

    Finish Summary layout

--- src/Summary.js
+++ src/Summary.js
@@ -5,7 +5,7 @@ import imageSunny from './Clear.png'
 export const Summary = ({temperature, date, location, weatherLabel}) => {
     return (
         <div className='summaryPanel'>
-            <div> <img src={imageSunny} alt='Weather icon'/> </div>
+            <img src={imageSunny} alt='Weather icon'/>
             <div className='temperature'>{Math.round(temperature)}<small>°C</small></div>
             <div className='weatherLabel'>{weatherLabel}</div>
             <div>Today • {date}</div>
