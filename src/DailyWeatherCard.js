6bc36e27727bd3db37fe7ebbad12191f510c0cb9
Zsofia Florek <58311322+zsofiaflorek@users.noreply.github.com>
2021-06-02 19:16:13 +0200

    Fix timezones

--- src/DailyWeatherCard.js
+++ src/DailyWeatherCard.js
@@ -6,7 +6,10 @@ export const DailyWeatherCard = ({
     maxTemperature,
     weatherLabel,
 }) => {
-    let parsedDate = new Date(date);
+    const year = date.slice(0, 4);
+    const month = date.slice(5, 7);
+    const day = date.slice(8, 10);
+    const parsedDate = new Date(year, month, day);
     return (
         <div className={styles.weatherCard}>
             <div className={styles.date}>
