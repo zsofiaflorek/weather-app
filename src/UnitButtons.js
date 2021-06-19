b3cc74b4c58acfc84fa29dac162d205ea95ac4ab
Zsofia Florek <58311322+zsofiaflorek@users.noreply.github.com>
2021-06-19 12:38:13 +0200

    Create state method for unit switcher

--- src/UnitButtons.js
+++ src/UnitButtons.js
@@ -1,14 +1,34 @@
-import styles from "./UnitButtons.module.css"
+import styles from "./UnitButtons.module.css";
+import { useState } from "react";
 
-export function UnitButtons ({containerClassName})  {
+export function UnitButtons({ containerClassName }) {
+    const [unit, setUnit] = useState("celsius");
     return (
         <div className={containerClassName}>
-            <button className={styles.unitButton}>
+            <button
+                className={
+                    unit === "celsius"
+                        ? `${styles.unitButton} ${styles.active}`
+                        : styles.unitButton
+                }
+                onClick={() => {
+                    setUnit("celsius");
+                }}
+            >
                 °C
             </button>
-            <button className={styles.unitButton}>
+            <button
+                className={
+                    unit === "fahrenheit"
+                        ? `${styles.unitButton} ${styles.active}`
+                        : styles.unitButton
+                }
+                onClick={() => {
+                    setUnit("fahrenheit");
+                }}
+            >
                 °F
             </button>
         </div>
-    )
+    );
 }
