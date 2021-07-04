aea04d393d0da719f0025b54c2d30de2c5aff3ba
Zsofia Florek <58311322+zsofiaflorek@users.noreply.github.com>
2021-07-04 14:54:20 +0200

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
 import { Weather } from "./Weather.js";
+import {BrowserRouter, Route, Redirect} from "react-router-dom"
 
 export function App() {
     return (
+      <BrowserRouter>
         <div className="App">
+          <Route path="/weather/:locationId">
             <Weather />
+          </Route>
+          <Route path="/" exact>
+            <Redirect to="/weather/615702"/>
+          </Route>
         </div>
+      </BrowserRouter>
     );
 }
