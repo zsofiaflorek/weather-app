09b44f0ee199ebbf38933f89106846c8a8ea97bb
Zsofia Florek <58311322+zsofiaflorek@users.noreply.github.com>
2021-07-08 16:21:03 +0200

    Add CSS to location search page

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,26 +1,34 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useLocationSearch } from "./utils/data-access";
+import styles from "./LocationCard.module.css";
 
 export const SearchPage = () => {
     const [search, setSearch] = useState("");
     const { searchResult } = useLocationSearch(search);
     return (
-        <div>
-            <input
-                type="text"
-                placeholder="search location"
-                value={search}
-                onChange={(e) => {
-                    setSearch(e.currentTarget.value);
-                }}
-            />
+        <div className={styles.locationDisplay}>
+          <i className="fas fa-times"/>
+          <input
+            type="text"
+            placeholder="search location"
+            value={search}
+            onChange={(e) => {
+              setSearch(e.currentTarget.value);
+            }}
+          />
             {searchResult !== undefined ? (
-                <ul>
+                <ul className={styles.locationList}>
                     {searchResult.slice(0, 10).map((result) => {
                         return (
-                            <li key={result.woeid}>
-                                <Link to={`/weather/${result.woeid}`}>
+                            <li
+                                key={result.woeid}
+                                className={styles.locationListItem}
+                            >
+                                <Link
+                                    to={`/weather/${result.woeid}`}
+                                    className={styles.locationResult}
+                                >
                                     {result.title}
                                 </Link>
                             </li>
