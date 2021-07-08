b1f333d1dc67c98b3d7436463a10ccbe944741b5
Zsofia Florek <58311322+zsofiaflorek@users.noreply.github.com>
2021-07-08 16:54:04 +0200

    Fix the icon search on summary.

--- src/Summary.js
+++ src/Summary.js
@@ -1,7 +1,15 @@
 import "./Summary.css";
-import imageSunny from "./Clear.png";
-import {calculateTemperature, formatUnit} from "./utils/temperature"
-
+import { calculateTemperature, formatUnit } from "./utils/temperature";
+import showerIcon from "./Shower.png";
+import lightRainIcon from "./LightRain.png";
+import sleetIcon from "./Sleet.png";
+import snowIcon from "./Snow.png";
+import thunderstormIcon from "./Thunderstorm.png";
+import lightCloudIcon from "./LightCloud.png";
+import heavyRainIcon from "./HeavyRain.png";
+import hailIcon from "./Hail.png";
+import clearIcon from "./Clear.png";
+import heavyCloudIcon from "./HeavyCloud.png";
 
 export const Summary = ({
     temperature,
@@ -12,7 +20,10 @@ export const Summary = ({
 }) => {
     return (
         <div className="summaryPanel">
-            <img src={imageSunny} alt="Weather icon" />
+            <img
+                src={getIconForWeatherLabel(weatherLabel)}
+                alt="Weather icon"
+            />
             <div className="temperature">
                 {calculateTemperature(unit, temperature)}
                 <small>{formatUnit(unit)}</small>
@@ -25,3 +36,20 @@ export const Summary = ({
         </div>
     );
 };
+
+const iconForLabel = {
+    Showers: showerIcon,
+    "Light Rain": lightRainIcon,
+    Sleet: sleetIcon,
+    Snow: snowIcon,
+    Thunderstorm: thunderstormIcon,
+    "Light Cloud": lightCloudIcon,
+    "Heavy Rain": heavyRainIcon,
+    Hail: hailIcon,
+    Clear: clearIcon,
+    HeavyCloud: heavyCloudIcon,
+};
+
+function getIconForWeatherLabel(weatherLabel) {
+    return iconForLabel[weatherLabel] ?? heavyCloudIcon;
+}
