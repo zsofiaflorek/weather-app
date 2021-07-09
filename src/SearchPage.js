10ad56ba4c6a6b00f5c053852a6fc46f7617b9b2
Zsofia Florek <58311322+zsofiaflorek@users.noreply.github.com>
2021-07-09 15:39:34 +0200

    Add go back method to search location page

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,22 +1,28 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { useLocationSearch } from "./utils/data-access";
 import styles from "./LocationCard.module.css";
 
 export const SearchPage = () => {
     const [search, setSearch] = useState("");
     const { searchResult } = useLocationSearch(search);
+    let history = useHistory();
+    function handleClick() {
+        history.goBack();
+    }
     return (
         <div className={styles.locationDisplay}>
-          <i className="fas fa-times"/>
-          <input
-            type="text"
-            placeholder="search location"
-            value={search}
-            onChange={(e) => {
-              setSearch(e.currentTarget.value);
-            }}
-          />
+            {history.action === "PUSH" ? (
+                <i className="fas fa-times" onClick={handleClick} />
+            ) : null}
+            <input
+                type="text"
+                placeholder="search location"
+                value={search}
+                onChange={(e) => {
+                    setSearch(e.currentTarget.value);
+                }}
+            />
             {searchResult !== undefined ? (
                 <ul className={styles.locationList}>
                     {searchResult.slice(0, 10).map((result) => {
