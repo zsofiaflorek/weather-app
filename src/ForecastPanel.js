a8d7c0922c865c6a19a8d7458cbc55978fc249c8
Zsofia Florek <58311322+zsofiaflorek@users.noreply.github.com>
2021-07-22 19:48:22 +0200

    Fix Prettier issues

--- src/ForecastPanel.js
+++ src/ForecastPanel.js
@@ -1,9 +1,5 @@
-import styles from "./ForecastPanel.module.css"
+import styles from "./ForecastPanel.module.css";
 
-export const ForecastPanel = ({children}) => {
-    return (
-        <div className={styles.forecastPanel}>
-            {children}
-        </div>
-    )
-}
+export const ForecastPanel = ({ children }) => {
+    return <div className={styles.forecastPanel}>{children}</div>;
+};
